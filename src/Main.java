ee61cb86158be69816cc8204de9a0ecff35511b9
perkles <ph997595305@gmail.com>
2022-10-11 13:29:31 -0300

    Desafio BuscaSequencial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner number = new Scanner(System.in);
+
+        int num = number.nextInt();
+
+        if(num == 3) {
+            System.out.println("Fizz");
+        }else if(num == 5) {
+            System.out.println("Buzz");
+        }if(num == 15) {
+            System.out.println("FizzBuzz");
+        }else if(num == 4) {
+            System.out.println(num);
+        }
+
     }
 }
